08b585cd3e7b9a5561f0cd67c5025d7de332f04d
AhmedMedhatAamin <ahmedmedhat.ahmed011@gmail.com>
2023-06-16 19:49:04 -0700

    ahmed

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,18 +9,18 @@
  */
 int main(void)
 {
-    char lowercase = 'a';
-    char uppercase = 'A';
-    while (lowercase <= 'z')
-    {
-        putchar(lowercase);
-        lowercase++;
-    }
-    while (uppercase <= 'Z')
-    {
-        putchar(uppercase);
-        uppercase++;
-    }
-    putchar('\n');
-    return 0;
+	char lowercase = 'a';
+	char uppercase = 'A';
+	while (lowercase <= 'z')
+	{
+		putchar(lowercase);
+		lowercase++;
+	}
+	while (uppercase <= 'Z')
+	{
+		putchar(uppercase);
+		uppercase++;
+	}
+	putchar('\n');
+	return (0);
 }
