8daf502f19426818ca4ba9f589d5dd7d1b2508ca
AhmedMedhatAamin <ahmedmedhat.ahmed011@gmail.com>
2023-06-16 19:30:46 -0700

    ahmed

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: This program prints the alphabet in lowercase,
+ * followed by uppercase, using the putchar function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char lowercase = 'a';
+    char uppercase = 'A';
+
+    while (lowercase <= 'z')
+    {
+        putchar(lowercase);
+        lowercase++;
+    }
+
+    while (uppercase <= 'Z')
+    {
+        putchar(uppercase);
+        uppercase++;
+    }
+
+    putchar('\n');
+
+    return 0;
+}
