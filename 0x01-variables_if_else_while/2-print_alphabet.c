6b18dda2215c0d30c60cfaf3cde32421e65b1943
AhmedMedhatAamin <ahmedmedhat.ahmed011@gmail.com>
2023-06-18 10:19:07 -0700

    ahmed

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+
 int main() {
     char letter;
 
-    // Iterate from 'a' to 'z' and print each letter
+    /* Iterate from 'a' to 'z' and print each letter */
     for (letter = 'a'; letter <= 'z'; letter++) {
         putchar(letter);
     }
