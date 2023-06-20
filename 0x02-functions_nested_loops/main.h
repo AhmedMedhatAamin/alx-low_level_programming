7d61410cb130d978cac1d390a867433806086cc8
AhmedMedhatAamin <ahmedmedhat.ahmed011@gmail.com>
2023-06-20 16:57:44 -0700

    ahmed

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,19 +1,7 @@
-#include <limits.h>
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	int r;
+int _putchar(char c);
+int print_sign(int n);
 
-	r = print_sign(98);
-	if (r < 0)
-		r *= -1;
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
-}
+#endif /* MAIN_H */
