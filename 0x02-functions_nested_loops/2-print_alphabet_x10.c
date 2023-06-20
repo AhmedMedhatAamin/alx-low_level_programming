c64478b9c9e40c1a976741be32415bb72fd7e242
AhmedMedhatAamin <ahmedmedhat.ahmed011@gmail.com>
2023-06-20 16:20:18 -0700

    ahmed

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,15 +5,15 @@
  */
 void print_alphabet_x10(void)
 {
-    char letter;
-    int i;
+	char letter;
+	int i;
 
-    for (i = 0; i < 10; i++)
-    {
-        for (letter = 'a'; letter <= 'z'; letter++)
-        {
-            _putchar(letter);
-        }
-        _putchar('\n');
-    }
+	for (i = 0; i < 10; i++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}
 }
