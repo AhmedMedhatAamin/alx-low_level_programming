9830ebfbca4b7a3faef9fd1139dbd90257b77af8
AhmedMedhatAamin <ahmedmedhat.ahmed011@gmail.com>
2023-06-20 15:50:49 -0700

    ahmed

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
