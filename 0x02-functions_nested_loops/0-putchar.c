4ae5d1b5afd8709856ed30595353ea041665c32c
AhmedMedhatAamin <ahmedmedhat.ahmed011@gmail.com>
2023-06-20 15:29:02 -0700

    ahmed

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,6 @@
 #include "main.h"
-// function declaration
-int main(void)
+
+int _putchar(char c)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
-
-    return (0);
+  return write(1, &c, 1);
 }
-
