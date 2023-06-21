7a04960b10db0979497df3e06b1acb52799a1a1c
AhmedMedhatAamin <ahmedmedhat.ahmed011@gmail.com>
2023-06-21 15:21:42 -0700

    ahmed

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-	int i;
-	int sum = 0;
+    int i;
+    int sum = 0;
 
-	for (i = 0; i < 1024; i++)
-	{
-		if (i % 3 == 0 || i % 5 == 0)
-			sum += i;
-	}
+    for (i = 0; i < 1024; i++)
+    {
+        if (i % 3 == 0 || i % 5 == 0)
+            sum += i;
+    }
 
-	printf("%d\n", sum);
+    printf("%d\n", sum);
 
-	return 0;
+    return (0);
 }
