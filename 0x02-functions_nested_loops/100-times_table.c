ab12328e68cd68656519d9e17f97a1e5e0f17f1e
AhmedMedhatAamin <ahmedmedhat.ahmed011@gmail.com>
2023-06-21 15:17:01 -0700

    ahmed

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,30 +1,43 @@
 #include <stdio.h>
 
+void print_to_98(int n);
+
 /**
- * print_to_98 - Prints all natural numbers from n to 98.
- * @n: The starting number.
+ * main - check the code
+ *
+ * Return: Always 0.
  */
+int main(void)
+{
+    print_to_98(0);
+    print_to_98(98);
+    print_to_98(111);
+    print_to_98(81);
+    print_to_98(-10);
+    return 0;
+}
+
 void print_to_98(int n)
 {
-	int i;
+    int i;
 
-	if (n <= 98)
-	{
-		for (i = n; i <= 98; i++)
-		{
-			printf("%d", i);
-			if (i != 98)
-				printf(", ");
-		}
-	}
-	else
-	{
-		for (i = n; i >= 98; i--)
-		{
-			printf("%d", i);
-			if (i != 98)
-				printf(", ");
-		}
-	}
-	printf("\n");
+    if (n <= 98)
+    {
+        for (i = n; i <= 98; i++)
+        {
+            printf("%d", i);
+            if (i != 98)
+                printf(", ");
+        }
+    }
+    else
+    {
+        for (i = n; i >= 98; i--)
+        {
+            printf("%d", i);
+            if (i != 98)
+                printf(", ");
+        }
+    }
+    printf("\n");
 }
