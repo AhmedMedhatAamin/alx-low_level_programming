a3d8f7c177ba33e05411787b5c1726d0409d611b
AhmedMedhatAamin <ahmedmedhat.ahmed011@gmail.com>
2023-06-22 15:35:29 -0700

    ahmed

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a triangle of '#' characters
+ * @size: The size of the triangle
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j <= i; j++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
