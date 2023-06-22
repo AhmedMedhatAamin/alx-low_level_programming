eba09b15893b4358da97f0d16d00a3c10f813175
AhmedMedhatAamin <ahmedmedhat.ahmed011@gmail.com>
2023-06-22 12:14:39 -0700

    ahmed

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -5,7 +5,6 @@
  * @c: third integer
  * Return: largest number
  */
-
 int largest_number(int a, int b, int c)
 {
     int largest;
@@ -23,5 +22,5 @@ int largest_number(int a, int b, int c)
         largest = c;
     }
 
-    return (largest);
+    return largest;
 }
