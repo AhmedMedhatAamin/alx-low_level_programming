7b59037cdd7a4eaaa7b55d0dd39a7e7601ef801d
AhmedMedhatAamin <ahmedmedhat.ahmed011@gmail.com>
2023-06-26 11:40:42 -0700

    ahmed

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+/**
+ * reset_to_98 - Updates the value pointed to by a pointer to 98
+ * @n: Pointer to an integer
+ *
+ * Description: This function takes a pointer to an integer as a parameter
+ * and updates the value it points to with the value 98.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function declarations (prototypes) */
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+/* Add your own function declarations here */
+
+#endif /* MAIN_H */
