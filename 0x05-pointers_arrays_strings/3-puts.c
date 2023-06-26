71cae7547e4d1e3d3867c8e99df0ed533a74c229
AhmedMedhatAamin <ahmedmedhat.ahmed011@gmail.com>
2023-06-26 15:44:45 -0700

    ahmed

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,18 +1,20 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _puts - Prints a string followed by a new line
- * @str: The string to be printed
+ * @str: The string to print
  */
 void _puts(char *str)
 {
 	int i = 0;
 
+	/* Iterate through the string until the null terminator is encountered */
 	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
+		putchar(str[i]);
 		i++;
 	}
-	_putchar('\n');
-}
 
+	putchar('\n');
+}
