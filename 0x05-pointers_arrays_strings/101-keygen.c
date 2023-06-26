dc58a10df3ead40a3dce8d19051b06cfb4d81b09
AhmedMedhatAamin <ahmedmedhat.ahmed011@gmail.com>
2023-06-26 15:20:17 -0700

    ahmed

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -13,15 +13,15 @@ int main(void)
 
     for (i = 0; i < PASSWORD_LENGTH; i++)
     {
-        int randomValue = rand() % 62;
+        int randomValue = rand() % 3;
         char nextChar;
 
-        if (randomValue < 26)
-            nextChar = 'a' + randomValue; /* Lowercase letter */
-        else if (randomValue < 52)
-            nextChar = 'A' + (randomValue - 26); /* Uppercase letter */
+        if (randomValue == 0)
+            nextChar = 'A' + (rand() % 26); /* Uppercase letter */
+        else if (randomValue == 1)
+            nextChar = 'a' + (rand() % 26); /* Lowercase letter */
         else
-            nextChar = '0' + (randomValue - 52); /* Digit */
+            nextChar = '0' + (rand() % 10); /* Digit */
 
         password[i] = nextChar;
     }
