63cf351f739f5d9f6421cb6b13e8a7ca65cf3e79
AhmedMedhatAamin <ahmedmedhat.ahmed011@gmail.com>
2023-07-05 16:34:25 -0700

    ahmed

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function prototypes */
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+
+
+#endif
