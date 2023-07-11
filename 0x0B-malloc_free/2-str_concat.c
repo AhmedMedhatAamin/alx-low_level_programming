6ef42120d8688a4fc74c366dc77b45e1935645f3
AhmedMedhatAamin <ahmedmedhat.ahmed011@gmail.com>
2023-07-11 15:40:46 -0700

    ahmed

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,15 +12,18 @@
  */
 char *str_concat(char *s1, char *s2)
 {
+	size_t len1, len2;
+	char *concatenated;
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	size_t len1 = strlen(s1);
-	size_t len2 = strlen(s2);
+	len1 = strlen(s1);
+	len2 = strlen(s2);
 
-	char *concatenated = malloc((len1 + len2 + 1) * sizeof(char));
+	concatenated = malloc((len1 + len2 + 1) * sizeof(char));
 
 	if (concatenated == NULL)
 		return (NULL);
